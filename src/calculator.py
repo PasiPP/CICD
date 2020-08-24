8646e70095b87b6669dd884b28822c703fdc1d1d
Viskaali <pasi.poisuo@gmail.com>
2020-08-24 14:42:08 +0300

    eka

--- src/calculator.py
+++ src/calculator.py
@@ -1,23 +1,13 @@
 
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('arguments', metavar='N', type=int, nargs='+',
-                   help='an integer for the accumulator')
-args = parser.parse_args()
-print(args.arguments)
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.add_argument('arguments', metavar='N', type=int, nargs='+',
+#                    help='an integer for the accumulator')
+# args = parser.parse_args()
+# print(args.arguments)
 
 def add(x, y):
     return x + y
 
-def subtract(x, y):
-    return x - y
 
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y == 0:
-        raise ValueError('ei voi jakaa')
-    return x / y
-
-print(add(args.arguments[0], args.arguments[1]))
+# print(add(args.arguments[0], args.arguments[1]))
